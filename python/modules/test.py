cdeecc4e59546b0708287657ce48d0d31bf4bb1e
shravan <shravanandoria21437@gmail.com>
2026-09-23 23:45:32 +0530

    Add initial package structure with math operations and example files

---
+++ python/modules/test.py
@@ -0,0 +1,11 @@
+from my_package.my_module import addition, substraction
+from my_package.sub_package.sub_module import multiply
+result = addition(10,5)
+print(result)
+
+
+result2 = substraction(10,5)
+print(result2)
+
+result3 = multiply(2,7)
+print(result3)
